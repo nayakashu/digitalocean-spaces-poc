0f8b151709c9df928b14c3b7cd5f3e795a5ff7d0
Ashutosh Nayak <ashutosh.nayak@tesco.com>
2020-08-11 00:28:07 +0530

    Setup express routes for static html files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import uploadRoutes from './routes/uploads';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(uploadRoutes);
 
 app.listen(3000, () => console.log('Hello World! @ 3000'));
